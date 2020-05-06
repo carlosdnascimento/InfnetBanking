7ed81becc1288fa0724986e60c6854196ba7f589
Carlos Pivotto <carlospivotto@msn.com>
2020-05-06 13:57:13 -0300

    Adicionar arquivos de projeto.

---
+++ InfnetMovieDataBase/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InfnetBanking
+{
+    class ContaBancaria
+    {
+        // Classes possuem membros
+        // Membros podem ser campos / propriedades ou métodos
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+        }
+
+        //Autopropriedade (C# 3.0 ou superior)
+        public string Titular { get; set; }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (_saldo >= valor)
+            {
+                _saldo -= valor;
+            } else
+            {
+                Console.WriteLine("Saldo insuficiente.");
+            }
+        }
+
+        public void Transferir(double valor, ContaBancaria conta)
+        {
+            // Programar as regras...
+        }
+    }
+}
---
+++ InfnetMovieDataBase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InfnetBanking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta1 = new ContaBancaria();
+            Console.Write("Informe o nome do titular: ");
+            conta1.Titular = Console.ReadLine();
+            Console.WriteLine(conta1.Titular + " | Saldo: R$ {0:0,0.00}", conta1.Saldo);
+            Console.Write("Informe o valor do depósito inicial: ");
+            double valor = Convert.ToDouble(Console.ReadLine());
+            conta1.Depositar(valor);
+            Console.WriteLine("Depositados R$" + valor);
+            Console.WriteLine(conta1.Titular + " | Saldo: R$ {0:0,0.00}", conta1.Saldo);
+            Console.Write("Informe o valor do saque: R$ ");
+            valor = Convert.ToDouble(Console.ReadLine());
+            conta1.Sacar(valor);
+            Console.WriteLine("Efetuado o saque de R$ {0:0,0.00}", valor);
+            Console.WriteLine(conta1.Titular + " | Saldo: R$ {0:0,0.00}", conta1.Saldo);
+        }
+    }
+}
